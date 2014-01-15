e00bf0e9be631732d6975a490b2d118a87c6a12c
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-01-15 20:22:48 +1100

    get annovar working

--- pipeline/pipeline_stages_config.groovy
+++ pipeline/pipeline_stages_config.groovy
@@ -315,15 +315,15 @@ qc_excel_report = {
 
 
 annovar_summarize_refgene = {
-    transform("av.refgene.exome_summary.csv","av.refgene.exonic_variant_function","av.refgene.genome_summary.csv") {
+    transform("vcf") to(["av","av.refgene.exome_summary.csv","av.refgene.exonic_variant_function","av.refgene.genome_summary.csv"]) {
         exec """
                 $ANNOVAR/convert2annovar.pl $input -format vcf4 > $output.av
 
                 $ANNOVAR/summarize_annovar.pl 
                     --genetype refgene 
-                    --verdbsnp 132  
-                    --outfile ${input.av}.refgene 
-                    --buildver hg19  $input.av $ANNOVAR/humandb/
+                    --verdbsnp 138  
+                    --outfile ${output.av}.refgene 
+                    --buildver hg19  $output.av $ANNOVAR/../humandb/
         """
     }
 }
