cd1e606723b38a18ee6eff8436f09cd8059a2409
Simon Sadedin <simon.sadedin@mcri.edu.au>
2014-01-16 17:34:04 +1100

    added dedicated bedtools sorted because bedtools only likes its own sorting

--- pipeline/scripts/reformat_bed_files.sh
+++ pipeline/scripts/reformat_bed_files.sh
@@ -8,6 +8,19 @@ for i in *.bed; do sed -i.bak 's/^/chr/' $i; done
 echo "Renaming chrMT to chrM ..."
 for i in *.bed; do sed -i.bak 's/^chrMT/chrM/' $i; done
 
+rm *.bak
+
+echo "Flattening ..."
+mkdir -p flattened
+for i in *.bed; do
+    sortBed -i $i | bedtools merge -nms -i - | sed 's/;.*$//' > flattened/$i
+done
+rm -rf unflattened
+mkdir -p unflattened
+mv *.bed unflattened
+mv flattened/*.bed .
+rmdir flattened
+
 echo "Sorting ..."
 mkdir sorted
 for i in *.bed; do 
@@ -18,15 +31,4 @@ mkdir -p unsorted
 mv *.bed unsorted
 mv sorted/*.bed .
 rmdir sorted
-
-echo "Flattening ..."
-mkdir -p flattened
-for i in *.bed; do
-    bedtools merge -nms -i $i | sed 's/;.*$//' > flattened/$i
-done
-rm -rf unflattened
-mkdir -p unflattened
-mv *.bed unflattened
-mv flattened/*.bed .
-rmdir flattened
 
