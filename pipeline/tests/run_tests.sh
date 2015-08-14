1b236ffcac446178d6908009874fec970c2f9076
Peter Georgeson <peter@supernifty.org>
2015-08-14 17:28:02 +1000

    test pipeline id generator

--- pipeline/tests/run_tests.sh
+++ pipeline/tests/run_tests.sh
@@ -1,3 +1,9 @@
-
 # assume we run from tests
+
+# python unit tests
+#python -m unittest discover -s . -p '*_test.py'
+python -m unittest discover -s . -p 'create_metadata_test.py'
+
+# bpipe tests
 ../../bpipe run ./dedup_test.groovy ./sample.bam
+
